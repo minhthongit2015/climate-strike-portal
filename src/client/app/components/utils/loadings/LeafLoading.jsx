eb2aea3f118572ef1338642ef192fc3cb2ade2dc
Minh Thong <thongnmtran@kms-technology.com>
2019-09-29 13:39:46 +0700

    improve loading component

--- src/client/app/components/utils/loadings/LeafLoading.jsx
+++ src/client/app/components/utils/loadings/LeafLoading.jsx
@@ -3,6 +3,7 @@ import { BreedingRhombusSpinner } from 'react-epic-spinners';
 import classnames from 'classnames';
 import './Loading.scss';
 
+
 export default props => (
   <div
     className={
@@ -12,5 +13,8 @@ export default props => (
       )}
   >
     <BreedingRhombusSpinner color="#4285f4" size={75} />
+    <div className="loading-text">
+      {(props.text || 'đang tải...').split('').map(c => <span key={Math.random()}>{c}</span>)}
+    </div>
   </div>
 );
